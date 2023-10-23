f590a59a4cedda108572156f875ff3c9d385c4e8
Guadeloupe33 <stevenfrancius@gmail.com>
2023-10-23 18:33:52 -0400

    added coment route

---
+++ Controllers/api/CommentRoutes.js
@@ -0,0 +1,23 @@
+
+const { Comment, User } = require('../../Models')
+const router = require('express').Router()
+
+
+router.post('/:id', async (req, res) => {
+  try {
+    const commentData = await Comment.create({
+      comment: req.body.comment,
+      blogpost_id: req.params.id,
+      user_id: req.session.user_id,
+    })
+
+    const findUser = await User.findByPk(req.session.user_id)
+
+    res.status(200).json({ commentData, findUser })
+  } catch (err) {
+    console.log(err)
+    res.status(500).json(err)
+  }
+})
+
+module.exports = router
