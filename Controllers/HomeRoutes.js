d1a9dd7ccca4dbae378d274174edb949e98b1d93
Guadeloupe33 <stevenfrancius@gmail.com>
2023-10-23 18:42:32 -0400

    added homeRoute to the project

---
+++ Controllers/HomeRoutes.js
@@ -0,0 +1,50 @@
+const { BlogPost, User, Comment } = require('../Models')
+const withAuth = require('../Utils/auth')
+const router = require('express').Router()
+
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/')
+    return
+  }
+
+  res.render('login')
+})
+
+router.get('/', withAuth, async (req, res) => {
+    try {
+      const blogPostData = await BlogPost.findAll({
+        include: [{ model: User }, { model: Comment, include: [{ model: User }] }],
+      })
+  
+      const BlogPosts = blogPostData.map((blogPost) => blogPost.get({ plain: true }))
+  
+      res.render('homepage', {
+        BlogPosts,
+        loggedIn: req.session.loggedIn,
+      })
+    } catch (err) {
+      res.status(500).json(err)
+    }
+  })
+
+router.get('/dashboard', withAuth, async (req, res) => {
+  try {
+    const BlogPostData = await BlogPost.findAll({
+      where: {
+        user_id: req.session.user_id,
+      },
+    })
+
+    const posts = BlogPostData.map((post) => post.get({ plain: true }))
+
+    res.render('dashboard', {
+      posts,
+      loggedIn: req.session.loggedIn,
+    })
+  } catch (err) {
+    res.status(500).json(err)
+  }
+})
+
+module.exports = router
