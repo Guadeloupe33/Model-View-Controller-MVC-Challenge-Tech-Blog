a99526857c04a47c77fac402cbad4881b59638cc
Guadeloupe33 <stevenfrancius@gmail.com>
2023-10-23 21:02:43 -0400

    added seed date to the project

---
+++ Seeds/userData.js
@@ -0,0 +1,22 @@
+[
+    {
+      "name": "Steven Francius",
+      "email": "stevenfrancius@gmail.com",
+      "password": "guadeloupe33"
+    },
+    {
+      "name": "Stessie Francius",
+      "email": "stessiefrancius@gmail.com",
+      "password": "guadeloupe333"
+    },
+    {
+      "name": "Linda Smith",
+      "email": "lindasmith@gmail.com",
+      "password": "guadeloupe3333"
+    },
+    {
+        "name": "Bill Gate",
+        "email": "billgate@gmail.com",
+        "password": "guadeloupe33333"
+      }
+  ]
