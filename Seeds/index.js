1f7005f9f14c6e393db8d361ce67438362f6bf44
Guadeloupe33 <stevenfrancius@gmail.com>
2023-10-26 08:17:23 -0400

    added some addtional funactionality

--- Seeds/index.js
+++ Seeds/index.js
@@ -1,25 +1,33 @@
 const sequelize = require("../config/connection");
 const { User, BlogPost } = require("../models");
 
+// Import user and blog post data from JSON files
 const userData = require("./userData.json");
 const blogPostData = require("./blogPostData.json");
 
+// Define the function to seed the database
 const seedDatabase = async () => {
+  // Synchronize the database by dropping and recreating the tables (use with caution)
   await sequelize.sync({ force: true });
 
+  // Bulk insert user data into the User table
   const users = await User.bulkCreate(userData, {
-    individualHooks: true,
-    returning: true,
+    individualHooks: true, // Trigger hooks (e.g., password hashing)
+    returning: true, // Get the inserted records
   });
 
+  // Loop through the blog post data and create blog posts
   for (const blog of blogPostData) {
+    // Create a blog post and associate it with a random user
     await BlogPost.create({
-      ...blog,
-      user_id: users[Math.floor(Math.random() * users.length)].id,
+      ...blog, // Spread the blog post data
+      user_id: users[Math.floor(Math.random() * users.length)].id, // Assign a random user ID
     });
   }
 
+  // Exit the script
   process.exit(0);
 };
 
+// Call the database seeding function
 seedDatabase();
--- Utils/auth.js
+++ Utils/auth.js
@@ -1,10 +1,15 @@
 const withAuth = (req, res, next) => {
-    if (!req.session.loggedIn) {
-      res.redirect('/login')
-      console.log('debugme')
-    } else {
-      next()
-    }
+  // Check if the user is not logged in
+  if (!req.session.loggedIn) {
+    // Redirect to the login page
+    res.redirect('/login');
+    // Logging statement for debugging (optional)
+    console.log('MIDELWEARTAFSAFAF');
+  } else {
+    // If the user is logged in, allow them to proceed to the next middleware or route
+    next();
   }
-  
-  module.exports = withAuth
+}
+
+// Export the withAuth middleware for use in routes
+module.exports = withAuth;
